502f4b4577e988a969badff4e50a8444b2ec5598
Jason Peng <jason.j.peng@gamil.com>
2019-07-30 23:01:29 -0700

    works

--- mbox2.js
+++ mbox2.js
@@ -6,7 +6,7 @@
 const readline = require('readline');
 // const stream = 
 const fs = require('fs');
-const buffer = [];
+var buffer = [];
 var  statusRev = false;
 // var revFile = process.argv[2]  + "-rev";
 var revFile = "rev.txt"
@@ -14,15 +14,16 @@ var revFile = "rev.txt"
 instream = fs.createReadStream( process.argv[2])
 
 const rl = readline.createInterface({
-    input: instream,
-    output: process.stdout
+    input: instream
+    // output: process.stdout
 });
-
+var lineCounter =0;
 rl.on("line", function(line) {
+    lineCounter++;
     buffer.push(line);
 // find the begin of messahe    
     if (!statusRev) {
-      if (line.trim(" ") == "") {
+      if (line.trim(" ") === "") {
         if (isBegin(buffer)) {
             writeBuffer(buffer);
             buffer=[];
@@ -31,50 +32,49 @@ rl.on("line", function(line) {
       }
     } else {
     // end of message -- and ?.?.?.    
-      if (isThreeDots(line) && buffer.slice(-1).pop().substring(0,2) === "--") {
+      if (isThreeDots(line) && buffer[buffer.length-2].substring(0,2) === "--") {
+          console.log("...........")
           writeReverX2(buffer);
           buffer = [];
           statusRev = false;
       }
     }
 
-    console.log("---  > ", line );
+    // console.log("---  > ", line );
 })
 
 rl.on('close', function () {
     console.log("Done");
 })
 
-// rl.question('What do you think of Node.js? ', (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//   rl.close();
-// });
-
 
 // Need to have "From ", "From: ", "Date: ", "Subject: "
 function isBegin(buffer) {
+
+    console.log(lineCounter, buffer);
+
   var testKeys  = [
       "From ", "From: ", "Date: ", "Subject: "
   ];
-  buffer.forEach(function(line) {
-      if (testKeys.length === 0) {
-          return true;
-      } else {
+  buffer.forEach(function(ln) {
           for(var i = 0; i<testKeys.length; i++) {
-           if(line.substring(0, testKeys[i].length) === testKeys[i]) {
+           if(ln.substring(0, testKeys[i].length) === testKeys[i]) {
               testKeys.splice(i, 1);
               i = testKeys.length; 
            }
-         }
-      }
-  })
-  return false;
+        }
+
+    })     
+    if (testKeys.length === 0) {      
+             return true;
+    } else {
+         return false;
+    }     
 }
 
 // find the pattern of  ?.?.?. such as 2.4.5.
 function isThreeDots(str) {
+    console.log("=====================================", str)
    var str1 = str.split(".");
    if (str1.length < 3) {
        return false;
@@ -85,12 +85,14 @@ function isThreeDots(str) {
         }
     }        
    }  
+   console.log("===========++++++ ", str)
    return true;  
 }
 
 function writeBuffer(buffer) {
+    console.log("write buffer ---->   ", buffer )
     buffer.forEach(function (line) {
-        fs.appendFileSync(revFile, line);
+        fs.appendFileSync(revFile, line + "\n");
     })  
 }
 
@@ -101,11 +103,11 @@ function writeReverX2(buffer) {
    secondLast = buffer.pop();
    
    while (buffer.length > 0) {
-    fs.appendFileSync(revFile, buffer.pop())
+    fs.appendFileSync(revFile, buffer.pop() + "\n")
    }
 
-   fs.appendFileSync(revFile, secondLast);
-   fs.appendFileSync(revFile, lastOne);
+   fs.appendFileSync(revFile, secondLast + "\n");
+   fs.appendFileSync(revFile, lastOne + "\n");
    
 }
 
